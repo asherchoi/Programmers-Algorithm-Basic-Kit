2c8befc6e0cc48006acd6f321105080eb3362eab
asher choi <tahun970@naver.com>
2019-07-07 22:57:03 +0900

    Update bs1_budget.py

--- bs1_budget.py
+++ bs1_budget.py
@@ -1,21 +1,14 @@
 
 def solution(budgets, M):
     left, right = 0, max(budgets)
-    temp = 0
     
     while right >= left:
         mid = (left+right) // 2
-        result=0
-        for b in budgets:
-            if mid > b:
-                result += b
-            else:
-                result += mid  
-        if result > M:
+        amount = sum([b if mid > b else mid for b in budgets])
+        if amount > M:
             right = mid - 1
         else:
-            if result >= temp:
-                answer = mid
+            answer = mid
             left = mid + 1
             
     return answer
@@ -24,4 +17,4 @@ def solution(budgets, M):
        
 print(solution([110, 120, 140, 150], 485)) #127
 print(solution([110, 120, 140, 150], 400)) #100
-print(solution([110, 120, 100, 100], 450))
+print(solution([110, 120, 100, 100], 450)) #120
